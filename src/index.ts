fe610a78bc8f123d1e42b6e461f7196b57266be7
Hexona <xuanjiap@lolicon.ac.cn>
2022-12-24 18:52:03 -0500

    feat: support more bitrate/format/etc

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { ArgumentParser } from 'argparse';
 import koice from 'koice';
-import { Stream } from 'stream';
+import { PassThrough, Stream } from 'stream';
 import * as fs from 'fs';
 import * as readline from 'readline/promises';
 import delay from 'delay';
+import ffmpeg from 'fluent-ffmpeg';
+
+process.argv[1] = "koice";
 
 const parser = new ArgumentParser({
     description: 'Stream local audio file to a KOOK voice channel, without the need rejoin the channel when switching songs',
@@ -27,25 +30,32 @@ parser.add_argument('-i', '--input', {
 
 const args = parser.parse_args()
 
-var fileP: fs.ReadStream | undefined;
+var fileP = new PassThrough();
+// var fileP: fs.ReadStream | undefined;
 var input: string = "";
+var ffmpegInstance: ffmpeg.FfmpegCommand | undefined;
+
+
+var previousStream: boolean = false;
+var lastRead: number = -1;
+
+const stream = new Stream.Readable({
+    read(size) {
+        return true;
+    },
+})
+
+var paused: boolean = false;
+var queue: string[] = [];
 
 (async () => {
     const voice = new koice(args.token);
 
     voice.connectWebSocket(args.channel);
 
-    const stream = new Stream.Readable({
-        read(size) {
-            return true;
-        },
-    })
-
     await voice.startStream(stream);
 
     console.log(`Connected to channel ${args.channel} as ${args.token}`);
-
-    var previousStream: boolean = false;
 
     var skipOnce = false;
     if (args.input) {
@@ -61,31 +71,112 @@ var input: string = "";
         if (skipOnce) skipOnce = false;
         else input = await rl.question('')
         if (fs.existsSync(input)) {
-            console.log(`Start streaming: "${input}"`);
-            // console.log(input);
             if (previousStream) {
-                previousStream = false;
-                fileP?.destroy();
-                await delay(50);
+                queue.push(input);
+                console.log(`Added ${input} to queue`);
+            } else {
+                await playback(input);
             }
-
-            // console.log(444444);
-            // console.log(5555555);
-            previousStream = true;
-            fileP = fs.createReadStream(input);
-            await delay(50);
-            fileP.on('readable', async () => {
-                var chunk;
-                while (previousStream && (chunk = fileP?.read(16 * 64))) {
-                    if (!previousStream) break;
-                    stream.push(chunk);
-                    await delay(125);
-                }
-                fileP?.close
-            })
         } else {
-            console.log(`Cannot find file "${input}"`);
+            switch (input.toLowerCase()) {
+                case 'skip':
+                    await next();
+                    break;
+                case 'pause':
+                    console.log("Stream paused");
+                    paused = true;
+                    break;
+                case 'resume':
+                    console.log("Stream resumed");
+                    paused = false;
+                    break;
+                case 'queue':
+                    console.log('Queue:\n    ' + queue.join('\n    '));
+                    break;
+                default:
+                    console.log(`Cannot recognize file or command "${input}"`);
+                    break;
+            }
         }
     }
 })()
 
+async function next() {
+    if (queue.length) {
+        const nextup = queue.shift();
+        console.log("Next up: " + nextup);
+        if (nextup) await playback(nextup);
+    } else {
+        console.log('Queue is empty!');
+    }
+}
+
+async function playback(file: string) {
+    // console.log(file);
+    if (previousStream) {
+        console.log(`Stopping prevoius stream...`);
+        previousStream = false;
+        await delay(200);
+        ffmpegInstance?.kill("SIGSTOP");
+        fileP?.removeAllListeners();
+        fileP?.destroy();
+        await delay(900);
+        fileP = new PassThrough();
+        // return;
+    }
+
+    // console.log(444444);
+    // console.log(5555555);
+    previousStream = true;
+    var fileC = fs.createReadStream(file);
+    // fileP = fs.createReadStream(file);
+    console.log(`Transcoding "${file}"`);
+    ffmpegInstance = ffmpeg()
+        .input(fileC)
+        .audioCodec('pcm_u8')
+        // .audioCodec('libopus')
+        // .audioCodec('libmp3lame')
+        .audioBitrate(128)
+        .audioChannels(2)
+        // .audioFrequency(44100)
+        .audioFrequency(48000)
+        .outputFormat('wav');
+    // .outputFormat('mp3');
+    // .outputFormat('opus');
+    ffmpegInstance
+        .stream(fileP)
+    await delay(200);
+    var bytes = 0;
+    var bfs: any[] = [];
+    fileP.on('data', (chunk) => {
+        bfs.push(chunk)
+    })
+    fileP.on('end', async () => {
+        var now = 0;
+        var buffer = Buffer.concat(bfs);
+        // var rate = 11025;
+        var rate = 24000;
+        while (Date.now() - lastRead < 2000);
+        console.log(`Start streaming: "${file}"`);
+        while (previousStream && now <= buffer.length) {
+            if (!paused) {
+                lastRead = Date.now();
+                const chunk = buffer.subarray(now, now + rate);
+                // console.log(file);
+                // console.log(chunk);
+                if (previousStream && now <= buffer.length) {
+                    stream.push(chunk);
+                }
+                else {
+                    return;
+                }
+                now += rate;
+            }
+            await delay(250);
+        }
+        console.log("Stream ended");
+        if (previousStream) {
+            await next();
+        }
+    });
+}
